43da6be678b4a6cc27f0ab70fdfe1fc3edb48908
jimmyh3 <jimmyhe3452@gmail.com>
2018-08-19 04:03:43 -0700

    Created Request class to represent the client's request.

---
+++ src/main/javaserver/httpmessages/Request.java
@@ -0,0 +1,77 @@
+package main.javaserver.httpmessages;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Request {
+
+    private String uri;
+    private String verb;
+    private String httpVersion;
+    private List<Byte> body;
+    private Map<String, String> headers;
+
+    public Request(Socket _clientSocket) {
+        init(_clientSocket);
+    }
+
+    private void init(Socket _clientSocket) {
+        try {
+            BufferedReader clientSocketIn = new BufferedReader(new InputStreamReader(_clientSocket.getInputStream()));
+            headers = new HashMap<>();
+            body = new ArrayList<>();
+
+            getSetStartLine(clientSocketIn);
+            getSetHeaders(clientSocketIn);
+            getSetBody(_clientSocket.getInputStream());
+        } catch (BadRequestException | IOException | ArrayIndexOutOfBoundsException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private void getSetStartLine(BufferedReader clientSocketIn) throws BadRequestException, IOException {
+        String startLine = clientSocketIn.readLine();
+        String[] startLineSplit = startLine.split(" ");
+
+        if (startLineSplit.length != 3)  {
+            throw new BadRequestException("Bad Request");
+        } else {
+            verb = startLineSplit[0];
+            uri = startLineSplit[1];
+            httpVersion = startLineSplit[2];
+        }
+    }
+
+    private void getSetHeaders(BufferedReader clientSocketIn) throws IOException {
+        String headerLine = "";
+        
+        while (!(headerLine = clientSocketIn.readLine()).isEmpty()) {
+            String[] headerLineSplit = headerLine.split(":");
+            String headerName = headerLineSplit[0];
+            String headerValue = headerLineSplit[1];
+
+            headers.put(headerName, headerValue);
+        }
+    }
+
+    private void getSetBody(InputStream clientSocketIn) throws IOException {
+        byte readChar = 0;
+
+        if (headers.containsKey("Content-Length")) {
+            int contentLength = Integer.parseInt(headers.get("Content-Length").trim());
+
+            for (int i = 0; i < contentLength; i++) {
+                body.add((byte)clientSocketIn.read());
+            }
+        }
+    }
+
+}
