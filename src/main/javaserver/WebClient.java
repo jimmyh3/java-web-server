9c06b18bf4369e5de8f10217023918106e1ba8c1
jimmyh3 <jimmyhe3452@gmail.com>
2018-08-21 20:35:39 -0700

    Implemented Response.send(OutputStream) and added getters and setters. Created WebClient.testGetResponse() for testing basic Get Response to client)

--- src/main/javaserver/WebClient.java
+++ src/main/javaserver/WebClient.java
@@ -1,40 +1,67 @@
 package main.javaserver;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
+import java.util.Map;
 
 import main.javaserver.httpmessages.Request;
+import main.javaserver.httpmessages.Response;
 
 public class WebClient implements Runnable {
 
     private int id;
     private Socket clientSocket;
-    private PrintWriter clientSocketOut;    // output to client via output stream.
-    private BufferedReader clientSocketIn;  // get client input via input stream.
 
     public WebClient(int _id, Socket _clientSocket) {
         init(_id, _clientSocket);
     }
 
     private void init(int _id, Socket _clientSocket) {
+        id = _id;
+        clientSocket = _clientSocket;
+    }
+
+    @Override
+    public void run() {
+        Request request = new Request(clientSocket);
+        Response response = testGetResponse();
+
         try {
-            id = _id;
-            clientSocket = _clientSocket;
-            clientSocketOut = new PrintWriter(clientSocket.getOutputStream());
-            clientSocketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            response.send(clientSocket.getOutputStream());
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
         }
     }
 
-    @Override
-    public void run() {
-        boolean clientIsActive = true;
-        
-        Request request = new Request(clientSocket);
-        
+
+    private Response testGetResponse() {
+        Response response = new Response();
+
+        Map<String, String> headers = response.getHeaders();
+        List<Byte> body = response.getBody();
+        String htmlBody = "<html> <header><title>This is title</title></header><body> Hello world </body></html>";
+        byte[] htmlBodyBytes = htmlBody.getBytes();
+        int htmlBodySize = htmlBodyBytes.length;
+
+        response.setHttpVersion("HTTP/1.1");
+        response.setCode(200);
+        response.setReasonPhrase("OK");
+        headers.put("Date", "Mon, 27 Jul 2009 12:28:53 GMT");
+        headers.put("Server", "Apache/2.2.14 (Win32)");
+        headers.put("Connection", "Closed");
+        headers.put("Content-Length", Integer.toString(htmlBodySize));
+        headers.put("Content-Type", "Closed");
+
+        for (int i = 0; i < htmlBodySize; i++) {
+            body.add(htmlBodyBytes[i]);
+        }
+
+        return response;
     }
+
 }
--- src/main/javaserver/httpmessages/Response.java
+++ src/main/javaserver/httpmessages/Response.java
@@ -1,24 +1,71 @@
 package main.javaserver.httpmessages;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Response {
 
+	private String httpVersion;
     private int code;
-    private String reasonPhrase;
-    //private Resource resource;
+	private String reasonPhrase;
+	private Map<String, String> headers;
+	private List<Byte> body;
+    private Resource resource;
 
     public Response() {
+		httpVersion = "HTTP/1.1";
+		code = 500;
+		reasonPhrase = "Internal Server Error";
+		headers = new HashMap<>();
+		body = new ArrayList<>();
+	}
 
-    }
+	public Response(Resource _resource) {
+		this();
+		resource = _resource;
+	}
 
-    /*
-    public Response (Resource _resource) {
-        
-    }
-    */
-    
-    public void send(OutputStream clientSocketOut) {
+	// Getters and setters
+	public String getHttpVersion() { return httpVersion; }
+	public void setHttpVersion(String httpVersion) { this.httpVersion = httpVersion; }
+	public int getCode() { return code; }
+	public void setCode(int code) { this.code = code; }
+	public String getReasonPhrase() {return reasonPhrase;}
+	public void setReasonPhrase(String reasonPhrase) { this.reasonPhrase = reasonPhrase; }
+	public Map<String, String> getHeaders() { return headers; }
+	public void setHeaders(Map<String, String> headers) { this.headers = headers; }
+	public List<Byte> getBody() { return body; }
+	public void setBody(List<Byte> body) { this.body = body; }
 
-    }
+	public void send(OutputStream clientSocketOut) throws IOException {
+		BufferedOutputStream bfo = new BufferedOutputStream(clientSocketOut);
+		String startLine = String.format("%s %d %s\n", httpVersion, code, reasonPhrase);
+		String headerStr = getResponseHeaderString(headers);
+
+		bfo.write(startLine.getBytes());
+		bfo.write(headerStr.getBytes());
+		bfo.write("\n".getBytes());
+		
+		for (Byte bite : body) {
+			bfo.write(bite);
+		}
+
+		bfo.flush();
+	}
+	
+	private String getResponseHeaderString(Map<String, String> _headers) {
+		String headerStr = "";
+
+		for (Map.Entry<String, String> entry : _headers.entrySet()) {
+			headerStr += String.format("%s:%s\n", entry.getKey(), entry.getValue());
+		}
+
+		return headerStr;
+	}
 }
