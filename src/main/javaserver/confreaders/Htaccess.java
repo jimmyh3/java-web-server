9b7f518bbcac73524b7f0b7837da54ac74250398
jimmyh3 <jimmyhe3452@gmail.com>
2018-08-24 00:29:23 -0700

    Implemented Htaccess initialization.

--- src/main/javaserver/confreaders/Htaccess.java
+++ src/main/javaserver/confreaders/Htaccess.java
@@ -1,8 +1,63 @@
 package main.javaserver.confreaders;
 
-public class Htaccess {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Htaccess extends ConfigurationReader {
     
-    public Htaccess () {
-        
+    private Htpassword userFile;
+    private String authType;
+    private String authName;
+    private String require; 
+
+    public Htaccess(String fileName) throws FileNotFoundException {
+        super(fileName);
     }
+
+    public String getAuthType() {
+        return authType;
+    }
+
+    public String getAuthName() {
+        return authName;
+    }
+
+    public String getRequire() {
+        return require;
+    }
+
+    @Override
+    public void load() throws IOException {
+        String readLine = null;
+
+        while ((readLine = fileReader.readLine()) != null) {
+            readLine = readLine.trim();
+
+            if (readLine.charAt(0) != '#') {
+                String[] readLineSplit = readLine.split("\\s+");
+                String fieldName = readLineSplit[0];
+                String fieldValue = readLineSplit[1];
+
+                if (fieldName.equals("AuthUserFile")) {
+                    userFile = initializeHtpassword(fieldValue);
+                } else if (fieldName.equals("AuthType")) {
+                    authType = fieldValue;
+                } else if (fieldName.equals("AuthName")) {
+                    authName = fieldValue;
+                } else if (fieldName.equals("Require")) {
+                    require = fieldValue;
+                }
+            }
+        }
+
+    }
+
+    private Htpassword initializeHtpassword(String htpasswordFileName) {
+        //TODO: Implement this.
+
+        return null;
+    }
+
 }
--- src/test/TestConfigurationReaders.java
+++ src/test/TestConfigurationReaders.java
@@ -3,6 +3,7 @@ package test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import main.javaserver.confreaders.Htaccess;
 import main.javaserver.confreaders.HttpdConf;
 import main.javaserver.confreaders.MimeTypes;
 
@@ -25,10 +26,20 @@ public class TestConfigurationReaders {
         System.out.println(mimeTypes.getMimeType("m3u"));
         System.out.println(mimeTypes.getMimeType("djvu"));
     }
+
+    public void testHtaccess(String fileName) throws FileNotFoundException, IOException {
+        Htaccess htaccess = new Htaccess(fileName);
+        htaccess.load();
+
+        System.out.println(htaccess.getAuthName());
+        System.out.println(htaccess.getAuthType());
+        System.out.println(htaccess.getRequire());
+    }
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
         TestConfigurationReaders testConfigurationReaders = new TestConfigurationReaders();
         //testConfigurationReaders.testHttpConf("src/main/javaserver/conf/httpd.conf");
         testConfigurationReaders.testMimeTypes("src/main/javaserver/conf/mime.types");
+        testConfigurationReaders.testHtaccess("src/main/javaserver/conf/.htaccess");
     }
 }
