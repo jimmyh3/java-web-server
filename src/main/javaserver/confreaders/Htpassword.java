a0060e6b97b9c1f3b60d1c17715b11d0322e79c3
jimmyh3 <jimmyhe3452@gmail.com>
2018-08-26 15:33:43 -0700

    Implemented Htpassword to handle SHA only. Modified TestConfigurationReader to test for Htpassword, which confirms works for SHA.

--- src/main/javaserver/confreaders/Htpassword.java
+++ src/main/javaserver/confreaders/Htpassword.java
@@ -1,8 +1,63 @@
 package main.javaserver.confreaders;
 
-public class Htpassword {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Base64;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-    public Htpassword () {
-        
+public class Htpassword extends ConfigurationReader {
+
+    private Map<String, String> users;
+
+    public Htpassword(String fileName) throws FileNotFoundException {
+        super(fileName);
+
+        users = new HashMap();
     }
+
+    @Override
+    public void load() throws IOException {
+        String readLine = "";
+
+        while ((readLine = fileReader.readLine()) != null) {
+            readLine = readLine.trim();
+
+            if (readLine.charAt(0) != '#' && readLine.contains("{SHA}")) {
+                String[] userPassPair= readLine.replace("{SHA}", "").split(":");
+                String username = userPassPair[0].trim();
+                String passEncoded = userPassPair[1].trim();
+                
+                users.put(username, passEncoded);
+            }
+        }
+    }
+
+    public boolean isAuthorized(String userPassEncoded) throws NoSuchAlgorithmException {
+        String userPassDecoded = new String(Base64.getDecoder().decode(userPassEncoded), "UTF-8");
+        String[] userPassPair = userPassDecoded.split(":");
+        String username = userPassPair[0].trim();
+        String password = userPassPair[1].trim();
+
+        return isAuthorized(username, password);
+    }
+
+    public boolean isAuthorized(String username, String password) throws NoSuchAlgorithmException {
+        boolean isAuthorized = false;
+
+        if (users.containsKey(username)) {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            String passwordHashed = Base64.getEncoder().encodeToString(md.digest(password.getBytes()));
+            String passwordStored = users.get(username);
+
+            isAuthorized = passwordHashed.equals(passwordStored);
+        } else {
+            System.out.println("Htpassword: no such user found.");
+        }
+
+        return isAuthorized;
+    }
+
 }
--- src/test/TestConfigurationReaders.java
+++ src/test/TestConfigurationReaders.java
@@ -2,11 +2,13 @@ package test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import main.javaserver.confreaders.Htaccess;
+import main.javaserver.confreaders.Htpassword;
 import main.javaserver.confreaders.HttpdConf;
 import main.javaserver.confreaders.MimeTypes;
 
@@ -16,34 +18,45 @@ public class TestConfigurationReaders {
         HttpdConf httpdConf = new HttpdConf(fileName);
         httpdConf.load();
 
-        System.out.println(httpdConf.getConfigValue("Listen")[0]);
-        System.out.println(httpdConf.getConfigValue("DocumentRoot")[0]);
-        System.out.println(httpdConf.getConfigValue("/ab/")[0]);
+        System.out.println("testHttpConf.getConfigValue('Listen')  = " + httpdConf.getConfigValue("Listen")[0]);
+        System.out.println("testHttpConf.getConfigValue('DocumentRoot')  = " + httpdConf.getConfigValue("DocumentRoot")[0]);
+        System.out.println("testHttpConf.getConfigValue('/ab/')  = " + httpdConf.getConfigValue("/ab/")[0]);
     }
 
     public void testMimeTypes(String fileName) throws FileNotFoundException, IOException {
         MimeTypes mimeTypes = new MimeTypes(fileName);
         mimeTypes.load();
 
-        System.out.println(mimeTypes.getMimeType("jpeg"));
-        System.out.println(mimeTypes.getMimeType("m3u"));
-        System.out.println(mimeTypes.getMimeType("djvu"));
+        System.out.println("testMimeTypes.getMimeType(jpeg) " + mimeTypes.getMimeType("jpeg"));
+        System.out.println("testMimeTypes.getMimeType(m3u) " + mimeTypes.getMimeType("m3u"));
+        System.out.println("testMimeTypes.getMimeType(djvu) " + mimeTypes.getMimeType("djvu"));
     }
 
     public void testHtaccess(String fileName) throws FileNotFoundException, IOException {
         Htaccess htaccess = new Htaccess(fileName);
         htaccess.load();
 
-        System.out.println(htaccess.getAuthName());
-        System.out.println(htaccess.getAuthType());
-        System.out.println(htaccess.getRequire());
+        System.out.println("testHtaccess.getAuthName(): " + htaccess.getAuthName());
+        System.out.println("testHtaccess.getAuthType(): " + htaccess.getAuthType());
+        System.out.println("testHtaccess.getRequire(): " + htaccess.getRequire());
+    }
+
+    public void testHtpassword(String fileName) throws FileNotFoundException, IOException, NoSuchAlgorithmException {
+        Htpassword htpassword = new Htpassword(fileName);
+        htpassword.load();
+
+        System.out.println("testHtpassword.isAuthorized('jimmy', 'password1234'): " + htpassword.isAuthorized("jimmy", "password1234"));
     }
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, NoSuchAlgorithmException {
         TestConfigurationReaders testConfigurationReaders = new TestConfigurationReaders();
+        System.out.println("TESTING ------------ TestConfigurationReaders ------------ TESTING");
         testConfigurationReaders.testHttpConf("src/main/javaserver/conf/httpd.conf");
         testConfigurationReaders.testMimeTypes("src/main/javaserver/conf/mime.types");
         testConfigurationReaders.testHtaccess("src/main/javaserver/conf/.htaccess");
+        testConfigurationReaders.testHtpassword("src/main/javaserver/conf/.htpasswd");
         //String str = "AuthUserFile \"/Users/jrob/workspace/server/public_html/.htpasswd\"";
+
+        System.out.println("END TEST ------------ TestConfigurationReaders ------------ END TEST");
     }
 }
