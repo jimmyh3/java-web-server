632df12198498d34d123d88a141a074c4867211f
jimmyh3 <jimmyhe3452@gmail.com>
2018-08-28 09:36:02 -0700

    Modified Resources class to have getters/setters.

--- src/main/javaserver/httpmessages/Resource.java
+++ src/main/javaserver/httpmessages/Resource.java
@@ -1,14 +1,58 @@
 package main.javaserver.httpmessages;
 
+import main.javaserver.confreaders.HttpdConf;
+
 public class Resource {
 
     private String absolutePath;
     private boolean isScript;
     private boolean isProtected;
 
-    public Resource () {
+    public Resource (String uri, HttpdConf httpConf) {
         
     }
+
+	/**
+	 * @return the isProtected
+	 */
+	public boolean isProtected() {
+		return isProtected;
+	}
+
+	/**
+	 * @param isProtected the isProtected to set
+	 */
+	public void setIsProtected(boolean isProtected) {
+		this.isProtected = isProtected;
+	}
+
+	/**
+	 * @return the isScript
+	 */
+	public boolean isScript() {
+		return isScript;
+	}
+
+	/**
+	 * @param isScript the isScript to set
+	 */
+	public void setIsScript(boolean isScript) {
+		this.isScript = isScript;
+	}
+
+	/**
+	 * @return the absolutePath
+	 */
+	public String getAbsolutePath() {
+		return absolutePath;
+	}
+
+	/**
+	 * @param absolutePath the absolutePath to set
+	 */
+	public void setAbsolutePath(String absolutePath) {
+		this.absolutePath = absolutePath;
+	}
 
     /*
     public Resource (String _uri, HttpdConf _httpConf) {
