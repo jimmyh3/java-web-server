0d02abb22067da44a205249e9b37ce8890626b05
jimmyh3 <jimmyhe3452@gmail.com>
2018-09-04 16:21:35 -0700

    Fixed WebClient to run in a loop to continously listen for http requests from client. Before it was only running for one request.

--- src/main/javaserver/WebClient.java
+++ src/main/javaserver/WebClient.java
@@ -2,6 +2,7 @@ package main.javaserver;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -22,50 +23,31 @@ public class WebClient implements Runnable {
     private Socket clientSocket;
     private HttpdConf httpdConf;
     private MimeTypes mimeTypes;
+    private boolean isRunning;
 
     public WebClient(int _id, Socket _clientSocket, HttpdConf _httpdConf, MimeTypes _mimeTypes) {
         id = _id;
         clientSocket = _clientSocket;
         httpdConf = _httpdConf;
         mimeTypes = _mimeTypes;
+        isRunning = true;
     }
-
+ 
     @Override
     public void run() {
-        Request request = new Request(clientSocket);
-        Response response = testGetResponse();
-        
-        try {
-            response.send(clientSocket.getOutputStream());
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
+        while (isRunning) {
+            try {
+                Request request = new Request(clientSocket);
+                Resource resource = new Resource(request.getURI(), httpdConf);
+                Response response = ResponseFactory.getResponse(request, resource, mimeTypes);
+    
+                response.send(clientSocket.getOutputStream());
+            } catch (FileNotFoundException ex) {
+                System.err.println(ex.getMessage());    
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
         }
-    }
-
-
-    private Response testGetResponse() {
-        Response response = new Response();
-
-        Map<String, String> headers = response.getHeaders();
-        List<Byte> body = response.getBody();
-        String htmlBody = "<html> <header><title>This is title</title></header><body> Hello world </body></html>";
-        byte[] htmlBodyBytes = htmlBody.getBytes();
-        int htmlBodySize = htmlBodyBytes.length;
-
-        response.setHttpVersion("HTTP/1.1");
-        response.setCode(200);
-        response.setReasonPhrase("OK");
-        headers.put("Date", "Mon, 27 Jul 2009 12:28:53 GMT");
-        headers.put("Server", "Apache/2.2.14 (Win32)");
-        headers.put("Connection", "Closed");
-        headers.put("Content-Length", Integer.toString(htmlBodySize));
-        headers.put("Content-Type", "Closed");
-
-        for (int i = 0; i < htmlBodySize; i++) {
-            body.add(htmlBodyBytes[i]);
-        }
-
-        return response;
     }
 
 }
