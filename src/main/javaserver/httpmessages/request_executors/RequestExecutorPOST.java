3fb0e21123957289f13fc15093d23ea69da2ea64
jimmyh3 <jimmyhe3452@gmail.com>
2018-09-07 20:44:51 -0700

    For RequestExecutablePOST, wrote simple skeleton for it; .getScriptContent() needs to be completed.

--- src/main/javaserver/httpmessages/request_executors/RequestExecutorPOST.java
+++ src/main/javaserver/httpmessages/request_executors/RequestExecutorPOST.java
@@ -1,7 +1,29 @@
 package main.javaserver.httpmessages.request_executors;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.QDecoderStream;
+
+import main.javaserver.confreaders.HttpdConf;
+import main.javaserver.confreaders.MimeTypes;
+import main.javaserver.httpmessages.Request;
+import main.javaserver.httpmessages.Response;
 import main.javaserver.httpmessages.request_executors.RequestExecutor;
 
 public class RequestExecutorPOST extends RequestExecutor {
+
+    private List<Byte> getScriptContent(Request request, Resource resource, HttpdConf httpdConf, MimeTypes mimeTypes) throws IOException {
+        //TODO: run the script and pass the POST request data to it.
+        //NOTE: scripts will handle the Response heads + content body as output. (right?)
+    }
     
+    
+    public Response execute(Request request, Resource resource, HttpdConf httpdConf, MimeTypes mimeTypes) throws IOException {
+        Response response = super.getInitializedResponse(request, resource, httpdConf, mimetypes);
+        response = getScriptContent(request, resource, httpdConf, mimeTypes);
+
+        response.setCode(200);
+        response.setReasonPhrase("OK");
+
+        return response;
+    }
+
 }
