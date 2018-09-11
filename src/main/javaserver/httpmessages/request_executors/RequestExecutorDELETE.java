1b5d42f00544e50ea97a757bbc247139c09d321a
jimmyh3 <jimmyhe3452@gmail.com>
2018-09-11 14:06:14 -0700

    Implemented HTTP Delete Request. This only returns 201 No Content response. Other responses such as 204 and 202 is left alone for now.

--- src/main/javaserver/httpmessages/request_executors/RequestExecutorDELETE.java
+++ src/main/javaserver/httpmessages/request_executors/RequestExecutorDELETE.java
@@ -1,7 +1,26 @@
 package main.javaserver.httpmessages.request_executors;
 
+import java.io.File;
+import java.io.IOException;
+
+import main.javaserver.confreaders.MimeTypes;
+import main.javaserver.httpmessages.Request;
+import main.javaserver.httpmessages.Resource;
+import main.javaserver.httpmessages.Response;
 import main.javaserver.httpmessages.request_executors.RequestExecutor;
 
 public class RequestExecutorDELETE extends RequestExecutor {
     
+    @Override
+    protected Response serve(Response initializedResponse, Request request, Resource resource, MimeTypes mimeTypes) throws IOException {
+        Response response = initializedResponse;
+        File file = new File(resource.getAbsolutePath());
+        
+        if (file.delete()) {
+            response.setCode(204);
+            response.setReasonPhrase("No Content");
+        }
+
+        return response;
+    }
 }
