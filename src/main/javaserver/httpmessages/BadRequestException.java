9b1f487f7d54f57a7c02dade7154fdbf1bb363a0
jimmyh3 <jimmyhe3452@gmail.com>
2018-09-11 23:51:34 -0700

    Fix warning in BadRequestException stating it's missing a serialVersionUID. This is not a halting error, just a compiler warning.

--- src/main/javaserver/httpmessages/BadRequestException.java
+++ src/main/javaserver/httpmessages/BadRequestException.java
@@ -2,6 +2,8 @@ package main.javaserver.httpmessages;
 
 public class BadRequestException extends Exception {
 
+    private static final long serialVersionUID = 1L;
+
     public BadRequestException(String ex) {
         super(ex);
     }
