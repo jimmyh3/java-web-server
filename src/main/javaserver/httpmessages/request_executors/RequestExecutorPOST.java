acdbad98335e0900f9384a6b2603717a1605513b
jimmyh3 <jimmyhe3452@gmail.com>
2018-09-20 08:06:57 -0700

    For RequestExecutorPOST, removed .getScriptContent() as RequestExecutor.handleScriptExecution()

--- src/main/javaserver/httpmessages/request_executors/RequestExecutorPOST.java
+++ src/main/javaserver/httpmessages/request_executors/RequestExecutorPOST.java
@@ -11,17 +11,12 @@ import main.javaserver.httpmessages.Response;
 import main.javaserver.httpmessages.request_executors.RequestExecutor;
 
 public class RequestExecutorPOST extends RequestExecutor {
-
-    private List<Byte> getScriptContent(Request request, Resource resource, MimeTypes mimeTypes) throws IOException, ParseException {
-        //TODO: run the script and pass the POST request data to it.
-        //NOTE: scripts will handle the Response heads + content body as output. (right?)
-    }
+    
     
     @Override
     protected Response serve(Response initializedResponse, Request request, Resource resource, MimeTypes mimeTypes) throws IOException {
+        // NOTE: POST body is being handled by RequestExecutor as a Script execution call.
         Response response = initializedResponse;
-        response = getScriptContent(request, resource, mimeTypes);
-
         response.setCode(200);
         response.setReasonPhrase("OK");
 
